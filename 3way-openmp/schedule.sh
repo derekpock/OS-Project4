0faa92f1636d141b0769e3a22f36616d4d6e85cc
Derek Pockrandt <derekpock@eos.beocat.ksu.edu>
2018-04-26 11:32:48 -0500

    Fixed files more

--- 3way-openmp/schedule.sh
+++ 3way-openmp/schedule.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sbatch --time=1:00:00 --constraint=elf --nodes=1 --ntasks-per-node=$1 --mem=4GB ./run.sh $1 $2
+sbatch --time=1:00:00 --constraint=elves --nodes=1 --ntasks-per-node=$1 --mem=4GB -- ./run.sh $1 $2
