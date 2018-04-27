2d2d3f85e94f114d666af9382664605e5b0f6d08
Derek Pockrandt <derekpock@gmail.com>
2018-04-27 17:28:57 -0500

    Reduced core count search

--- 3way-openmp/dwarves/superbatch.sh
+++ 3way-openmp/dwarves/superbatch.sh
@@ -15,14 +15,14 @@ do
     sbatch --time=1:00:00 --constraint=dwarves --nodes=1 --ntasks-per-node=2 --mem=4GB -- ./run.sh 2 1000 1
     for j in 1000 10000 100000 500000 1000000
     do
-        for k in 4 8 16 32
+        for k in 4 8 #16 32
         do
             sbatch --time=1:00:00 --constraint=dwarves --nodes=1 --ntasks-per-node=$k --mem=4GB -- ./run.sh $k $j 1
         done
         sbatch --time=1:00:00 --constraint=dwarves --nodes=2 --ntasks-per-node=4 --mem=2GB -- ./run.sh 4 $j 2
         sbatch --time=1:00:00 --constraint=dwarves --nodes=4 --ntasks-per-node=4 --mem=1GB -- ./run.sh 4 $j 4
         sbatch --time=1:00:00 --constraint=dwarves --nodes=4 --ntasks-per-node=2 --mem=1GB -- ./run.sh 2 $j 4
-        sbatch --time=1:00:00 --constraint=dwarves --nodes=2 --ntasks-per-node=16 --mem=2GB -- ./run.sh 16 $j 2
-        sbatch --time=1:00:00 --constraint=dwarves --nodes=16 --ntasks-per-node=2 --mem=512M -- ./run.sh 2 $j 16
+        #sbatch --time=1:00:00 --constraint=dwarves --nodes=2 --ntasks-per-node=16 --mem=2GB -- ./run.sh 16 $j 2
+        #sbatch --time=1:00:00 --constraint=dwarves --nodes=16 --ntasks-per-node=2 --mem=512M -- ./run.sh 2 $j 16
     done
 done
